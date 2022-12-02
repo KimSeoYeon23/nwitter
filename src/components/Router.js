dae167593518478d82e732f5f8968bf1a74ef765
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-02 15:03:39 +0900

    중간 업로드

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+// react-router-dom v6 위로는 Switch가 이름이 Routes로 변경됨
+
+const AppRouter = ({isLoggedIn}) => {
+
+    return (
+        <Router>
+            <Routes>
+                {
+                    isLoggedIn
+                    ? <Route exact path="/" element={<Home/>} />
+                    : <Route exact path="/" element={<Auth/>} />
+                }
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
