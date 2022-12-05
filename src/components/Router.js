aede664ce3e69a62f13cad98e98d899aaae6985c
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-05 15:14:39 +0900

    log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 // react-router-dom v6 위로는 Switch가 이름이 Routes로 변경됨
 
 const AppRouter = ({isLoggedIn}) => {
 
+
     return (
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Routes>
                 {
-                    isLoggedIn
-                    ? <Route exact path="/" element={<Home/>} />
-                    : <Route exact path="/" element={<Auth/>} />
+                    isLoggedIn ? (
+                        <>
+                            <Route exact path="/" element={<Home/>} />
+                            <Route exact path="/profile" element={<Profile/>} />
+                        </>
+                    ) : (
+                        <>
+                            <Route exact path="/" element={<Auth/>} />
+                        </>
+                    )
                 }
             </Routes>
         </Router>
