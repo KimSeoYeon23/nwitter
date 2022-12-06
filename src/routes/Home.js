f53b91a55e04845cd3844094649d2d596c7c21b9
SeoYeon <sollucidus0221@naver.com>
2022-12-06 21:10:00 +0900

    nweets update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { dbService } from 'fbase';
-import {addDoc, collection} from 'firebase/firestore';
+import {addDoc, collection, doc, getDocs, query} from 'firebase/firestore';
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const nweetCollection = collection(dbService, "nweets");
+    
+    const getNweets = async () => {
+        const nweetRef = await query(nweetCollection);
+        let dbNweets = await getDocs(nweetRef);
+
+        // 데이터 가져오기
+        dbNweets.forEach((doc) => {
+            setNweets(prev => [doc.data(), ...prev]);
+            console.log(nweets);
+        })
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     const onSubmit = async (e) => {
         e.preventDefault();
         try{
+            // 데이터 추가
             await addDoc(collection(dbService, "nweets"), {
                 nweet,
                 created_at: Date.now(),
