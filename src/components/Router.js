ddbc94b82763873c18f4b1b0950934f50210ffea
SeoYeon <sollucidus0221@naver.com>
2022-12-07 20:58:39 +0900

    profile editing

--- src/components/Router.js
+++ src/components/Router.js
@@ -18,7 +18,7 @@ const AppRouter = ({isLoggedIn, userObj}) => {
                     isLoggedIn ? (
                         <>
                             <Route exact path="/" element={<Home userObj={userObj}/>} />
-                            <Route exact path="/profile" element={<Profile/>} />
+                            <Route exact path="/profile" element={<Profile userObj={userObj}/>} />
                         </>
                     ) : (
                         <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authService } from 'fbase';
+import { authService, dbService } from 'fbase';
+import { collection, getDocs, query } from 'firebase/firestore';
 
-const Profile = () => {
+const Profile = ({userObj}) => {
     const navigate = useNavigate();
+    const nweetCollection = collection(dbService, "nweets");
 
     const onLogOutClick = () => {
         authService.signOut();
         navigate('/',  { replace: true });
     }
 
+    const getMyNweets = async () => {
+        const nweets = await getDocs(nweetCollection);
+        console.log(nweets);
+    }
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+
     return (
         <>
+            <div>{userObj.email}</div>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     )
