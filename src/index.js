d9232212509645fd57d276a2fdecd05d38e70c1e
KimSeoYeon23 <sollucidus0221@naver.com>
2022-12-08 16:17:00 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import {authService} from 'fbase';
 import 'css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
